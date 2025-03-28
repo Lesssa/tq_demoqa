6acd9d3f7ef3668a95a6d6695727d45d5a1fbe03
Lisa <lizazakek@gmail.com>
2025-03-28 22:50:45 +0300

    Updated test_alert.py

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,14 +1,19 @@
-from pages.alerts_page import Alerts
+from pages.alert import Alert
+from pages.alerts_page import AlertsPage
 from pages.main_page import MainPage
 
 
 def test_about():
     main_page = MainPage()
     main_page.is_displayed()
-    main_page.go_to_alerts()
+    main_page.go_to_alerts_window()
 
-    alerts = Alerts()
-    alerts.is_displayed()
+    alerts_page = AlertsPage()
+    alerts_page.is_displayed()
+    alerts_page.get_go_to_alerts().click()
+    alerts_page.get_alert_button().click()
+    alert = Alert()
+    alert.accept()
 
 
 
